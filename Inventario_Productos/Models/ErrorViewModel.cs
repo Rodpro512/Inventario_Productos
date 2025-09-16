14b5f1a17c05d301afef266f43ec90e3e23b71a4
Rodpro512 <fajrod512@gmail.com>
2025-09-16 07:33:42 -0600

    Agregar archivos de proyecto.

---
+++ Inventario_Productos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Inventario_Productos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
